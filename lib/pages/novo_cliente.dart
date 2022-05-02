f62a976be35c73e344fe7c872c1b5eda2bd9a26e
Pablo Henrique Correa <pablohcl93@gmail.com>
2022-05-02 12:39:16 -0300

    Tela novo cliente em construção 2

--- lib/pages/novo_cliente.dart
+++ lib/pages/novo_cliente.dart
@@ -10,7 +10,94 @@ class NovoCliente extends StatelessWidget {
         appBar: AppBar(
           title: Text('Novo Cliente'),
         ),
+        body: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          'CPF',
+                          style: TextStyle(
+                            fontSize: 26,
+                          ),
+                        ),
+                        Text('Pessoa Física'),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: [
+                        Text(
+                          'CNPJ',
+                          style: TextStyle(
+                            fontSize: 26,
+                          ),
+                        ),
+                        Text('Pessoa Jurídica'),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Form(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Icon(
+                      Icons.person,
+                      size: 88,
+                      color: Colors.green,
+                    ),
+                    TextFormField(
+                      validator: (value){
+                        if(value!.isEmpty){
+                          return 'Insira um CPF válido!';
+                        } else if(!isCpf(value)){
+                          return 'CNPJ inválido!';
+                        }
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'CPF',
+                        labelStyle: TextStyle(color: Colors.green),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+  bool isCpf(String valor){
+
+    return true;
   }
 }
