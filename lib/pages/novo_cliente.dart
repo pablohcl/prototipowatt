51771fe16104d7ac366b8e4b763ec479bcad6ce2
Pablo Henrique Correa <pablohcl93@gmail.com>
2022-05-03 14:53:41 -0300

    Correção de bugs... Tela novo cliente  - rolagem implementada  - clear no campo implementado  - mensagem do alert atualizada  - clicar no mesmo tipo nao mostra mais o alert

--- lib/pages/novo_cliente.dart
+++ lib/pages/novo_cliente.dart
@@ -10,6 +10,9 @@ class NovoCliente extends StatefulWidget {
 class _NovoClienteState extends State<NovoCliente> {
   String? tipoCadastro = '';
 
+  late TextEditingController cpfController = TextEditingController();
+  late TextEditingController cnpjController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -17,94 +20,100 @@ class _NovoClienteState extends State<NovoCliente> {
         appBar: AppBar(
           title: Text('Novo Cliente'),
         ),
-        body: Column(
-          children: [
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    if (tipoCadastro == '') {
-                      tipoCadastro = 'fisica';
-                      setState(() {
-                        montaTela();
-                      });
-                    } else {
-                      mostrarConfirmacaoDeTroca('fisica');
-                    }
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: isCpf()
-                        ? MaterialStateProperty.all(Colors.green)
-                        : MaterialStateProperty.all(
-                            Colors.black.withOpacity(0.2)),
-                    elevation: isCpf()
-                        ? MaterialStateProperty.all(2)
-                        : MaterialStateProperty.all(0),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      children: [
-                        Text(
-                          'CPF',
-                          style: TextStyle(
-                            fontSize: 26,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      if (tipoCadastro == '') {
+                        tipoCadastro = 'fisica';
+                        setState(() {
+                          montaTela();
+                        });
+                      } else if(tipoCadastro == 'fisica'){
+                        return;
+                      } else {
+                        mostrarConfirmacaoDeTroca('fisica');
+                      }
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: isCpf()
+                          ? MaterialStateProperty.all(Colors.green)
+                          : MaterialStateProperty.all(
+                              Colors.black.withOpacity(0.2)),
+                      elevation: isCpf()
+                          ? MaterialStateProperty.all(2)
+                          : MaterialStateProperty.all(0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            'CPF',
+                            style: TextStyle(
+                              fontSize: 26,
+                            ),
                           ),
-                        ),
-                        Text('Pessoa Física'),
-                      ],
+                          Text('Pessoa Física'),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  width: 20,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    if(tipoCadastro == ''){
-                      tipoCadastro = 'juridica';
-                      setState(() {
-                        montaTela();
-                      });
-                    } else {
-                      mostrarConfirmacaoDeTroca('juridica');
-                    }
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: isCnpj()
-                        ? MaterialStateProperty.all(Colors.green)
-                        : MaterialStateProperty.all(
-                            Colors.black.withOpacity(0.2)),
-                    elevation: isCnpj()
-                        ? MaterialStateProperty.all(2)
-                        : MaterialStateProperty.all(0),
+                  SizedBox(
+                    width: 20,
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      children: [
-                        Text(
-                          'CNPJ',
-                          style: TextStyle(
-                            fontSize: 26,
+                  ElevatedButton(
+                    onPressed: () {
+                      if (tipoCadastro == '') {
+                        tipoCadastro = 'juridica';
+                        setState(() {
+                          montaTela();
+                        });
+                      } else if(tipoCadastro == 'juridica'){
+                        return;
+                      } else {
+                        mostrarConfirmacaoDeTroca('juridica');
+                      }
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: isCnpj()
+                          ? MaterialStateProperty.all(Colors.green)
+                          : MaterialStateProperty.all(
+                              Colors.black.withOpacity(0.2)),
+                      elevation: isCnpj()
+                          ? MaterialStateProperty.all(2)
+                          : MaterialStateProperty.all(0),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            'CNPJ',
+                            style: TextStyle(
+                              fontSize: 26,
+                            ),
                           ),
-                        ),
-                        Text('Pessoa Jurídica'),
-                      ],
+                          Text('Pessoa Jurídica'),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            montaTela(),
-          ],
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              montaTela(),
+            ],
+          ),
         ),
       ),
     );
@@ -147,6 +156,8 @@ class _NovoClienteState extends State<NovoCliente> {
                 height: 8,
               ),
               TextFormField(
+                keyboardType: TextInputType.number,
+                controller: cpfController,
                 validator: (value) {},
                 decoration: InputDecoration(
                   labelText: 'CPF',
@@ -176,6 +187,8 @@ class _NovoClienteState extends State<NovoCliente> {
                 height: 8,
               ),
               TextFormField(
+                keyboardType: TextInputType.number,
+                controller: cnpjController,
                 validator: (value) {},
                 decoration: InputDecoration(
                   labelText: 'CNPJ',
@@ -218,20 +231,28 @@ class _NovoClienteState extends State<NovoCliente> {
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
-        title: Text('Atenção!', style: TextStyle(color: Colors.red),),
-        content: Text(
-            'Tem certeza que deseja trocar o tipo de cliente? TODOS OS DADOS DIGITADOS SERÃO PERDIDOS!'),
+        title: Text(
+          'Deseja trocar o tipo de cliente?',
+          style: TextStyle(color: Colors.black),
+        ),
+        content: Text('TODOS OS DADOS DIGITADOS SERÃO PERDIDOS!',
+            style: TextStyle(color: Colors.red, fontWeight: FontWeight.w600)),
         actions: [
           TextButton(
             onPressed: () {
               Navigator.of(context).pop();
             },
-            child: Text('Cancelar', style: TextStyle(color: Colors.black),),
+            child: Text(
+              'Cancelar',
+              style: TextStyle(color: Colors.black),
+            ),
           ),
           TextButton(
             onPressed: () {
               Navigator.of(context).pop();
               tipoCadastro = texto;
+              cpfController.text = '';
+              cnpjController.text = '';
               setState(() {
                 montaTela();
               });
