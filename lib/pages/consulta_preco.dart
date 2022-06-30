d9719eceb2415d9a8a90a258e18f413539ede243
Pablo Henrique Correa <pablohcl93@gmail.com>
2022-06-30 12:21:22 -0300

    Desenvolvendo a tela de consulta de preços.

--- lib/pages/consulta_preco.dart
+++ lib/pages/consulta_preco.dart
@@ -1,23 +1,113 @@
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ConsultaPreco extends StatelessWidget {
+import '../objects/db_helper.dart';
+
+class ConsultaPreco extends StatefulWidget {
   const ConsultaPreco({Key? key}) : super(key: key);
 
   @override
+  State<ConsultaPreco> createState() => _ConsultaPrecoState();
+}
+
+class _ConsultaPrecoState extends State<ConsultaPreco> {
+  DbHelper helper = DbHelper();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 30),
-      color: Colors.blue,
-      child: ElevatedButton(
-        child: Column(
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Consultar preços"),
+        ),
+        body: Column(
           children: [
-            Icon(Icons.exit_to_app),
-            Text('Voltar'),
+            SizedBox(
+              height: 12,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 8,
+                ),
+                Expanded(
+                  flex: 7,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelStyle: TextStyle(color: Colors.green),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Expanded(
+                  flex: 3,
+                  child: ElevatedButton(
+                    child: Text(
+                      "BUSCAR",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        padding: MaterialStateProperty.all(
+                            EdgeInsets.symmetric(vertical: 18))),
+                  ),
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            // IMPLEMENTAR ListView.builder() ##########
           ],
         ),
-        onPressed: (){
-          Navigator.pop(context);
-        },
+      ),
+    );
+  }
+
+  Widget _produtoCard(BuildContext context, int index) {
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: [
+              Container(
+                width: 80,
+                height: 80,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/icon/boxes.png"),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: Column(
+                  children: [
+                    // Desenvolver o Future para ter a lista cm os produtos
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
